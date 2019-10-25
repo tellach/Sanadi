2d1bb97c4c9c35e286e55c64c678680876cfdf91
AMINE <34172848+tellach@users.noreply.github.com>
2019-10-25 12:47:11 +0200

    added navigation

---
+++ Client/sanadi/screens/Home2.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, FlatList, Image, TouchableOpacity } from 'react-native';
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={require('./../assets/images/1.png')} style={styles.ImageStyle} />
+
+            </View>
+
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+
+        //backgroundColor: '#03224c',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ImageStyle: {
+        width: 300,
+        height: 300,
+        alignSelf: "center"
+
+
+    }
+});
+
